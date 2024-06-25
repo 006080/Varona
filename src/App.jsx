f2c75ef570ba8fec38b705a38a5d75f9e3c1eb35
006080 <nataliyarodionova26@gmail.com>
2024-06-25 19:13:21 +0200

    done

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../components/Header";
+import Home from "../components/Home";
+import OurStory from "../components/OurStory";
+import OurService from "../components/OurService";
+import Blog from "../components/Blog";
+import Contacts from "../components/Contacts";
+import Shop from "../components/Shop";
+import Footer from "../components/Footer";
+import { CartProvider } from '../components/CartContext';
+
+const App = () => {
+  const [cartItems, setCartItems] = React.useState([]);
+
+  // const addToCart = (product) => {
+  //   setCartItems((prevItems) => {
+  //     const existingItem = prevItems.find((item) => item.name === product.name);
+  //     if (existingItem) {
+  //       return prevItems.map((item) =>
+  //         item.name === product.name
+  //           ? { ...item, quantity: item.quantity + product.quantity }
+  //           : item
+  //       );
+  //     } else {
+  //       return [...prevItems, product];
+  //     }
+  //   });
+  // };
+
+  return (
+    <CartProvider>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ourstory" element={<OurStory />} />
+        <Route path="/ourservice" element={<OurService />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/shop" element={<Shop/>} />
+      </Routes>
+      <Footer></Footer>
+    </Router>
+    </CartProvider>
+  );
+};
+
+export default App;
